424053e76556aeb4f803ca94b15e9ac963264db5
JoseGamez632 <jlgamez0@misena.edu.co>
2023-07-15 18:57:44 -0500

    ejercicio

--- principal/views.py
+++ principal/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 
 def hola_mundo(request):
-    context={}
+    nombre = "Jose",
+    apellido = "Gamez",
+    telefono = "83892394"
+
+    context={
+        'nombres':nombre,
+        'apellidos':apellido,
+        'telefonos':telefono
+
+    }
     return render(request, "index.html", context)
