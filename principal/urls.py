24bd7bb785f71af9798a910d90827286d3e054b9
JoseGamez632 <jlgamez0@misena.edu.co>
2023-08-17 23:31:08 -0500

    se agrega menu de inicio, ventas y usuarios

--- principal/urls.py
+++ principal/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-# from principal.views import inicial
+from principal.views import inicio
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),    
+    path('', inicio, name='inicio'),
     path('ventas/',include('ventas.urls')),
     path('compras/',include('compras.urls')),
     path('usuarios/',include('usuarios.urls')),
-    #path('index/',include('index.urls')),
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 
+def inicio(request):
+    nombre = "Jose"
+    apellido = "Gamez"
+    telefono = "83892394"
+
+    context={
+        'nombres':nombre,
+        'apellidos':apellido,
+        'telefonos':telefono
+
+    }
+    return render(request, "index.html", context)
 
 def ventas(request):
     context={
     }
     return render(request, "ventas.html", context)
 
-# def inicial(request):
- #   context={
-  #  }
-   # return render(request, "index.html", context)
+
+def usuarios(request):
+    context={
+    }
+    return render(request, "usuarios.html", context)
