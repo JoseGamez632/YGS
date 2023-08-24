4838b0ce52fbcdfd2377b36cc2b25667969a562b
Nuceleus <aaortizreng@gmail.com>
2023-08-23 20:52:43 -0500

    avance pestañas
    quedan montadas y cargando en el proyecto las pestañas de insumos, proveedores, productos y servicios.

--- principal/urls.py
+++ principal/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from principal.views import inicio, ventas, usuarios
+from principal.views import inicio, ventas, usuarios, insumos, productos, servicios, proveedores
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('', inicio, name='inicio'),
     path('ventas/', ventas, name='ventas'),
     path('usuarios/',usuarios, name='usuarios'),
+    path('insumos/',insumos, name='insumos'),
+    path('productos/',productos, name='productos'),
+    path('servicios/',servicios, name='servicios'),
+    path('proveedores/',proveedores, name='proveedores'),
 ]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from usuarios.views import usuarios
+from usuarios.views import usuarios #insumos, productos, servicios
 
 urlpatterns = [
     path('',usuarios,name="usuarios"),
     path('',usuarios,name="usuarios"),
+    #pensé que se necesitaban los paths aquí lo dejo por si en algun momento es necesario
+    # path('',insumos, name="insumos"),
+    # path('',productos, name="productos"),
+    # path('',servicios, name="servicios"),
 ]
