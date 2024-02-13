0c168cc159165141b0e007c512f743c6b6e77cf8
JoseGamez632 <jlgamez0@misena.edu.co>
2024-02-12 23:07:24 -0500

    ajustar a MER

--- compras/models.py
+++ compras/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from usuarios.models import Usuario
+from usuarios.models import Usuario, Insumo
 
-# Create your models here.
-class proveedor(models.Model): #Falta proveedor_id autoinclementable
+# Proveedor terminado
+class Proveedor(models.Model): 
     prov_id = models.AutoField(primary_key=True)
     prov_nombre = models.CharField(max_length=45, verbose_name="Nombre del proveedor")
     prov_nit = models.CharField(max_length=16, verbose_name="NIT del proveedor")
@@ -20,7 +20,7 @@ class proveedor(models.Model): #Falta proveedor_id autoinclementable
         return "%s %s" %(self.prov_nombre)
     
 
-class compra(models.Model):
+class Compra(models.Model):
     comp_id = models.AutoField(primary_key=True)
     comp_fecha = models.DateField(verbose_name="Fecha de compra")
     comp_nombre_proveedor = models.CharField(max_length=45, verbose_name="Nombre  del Proveedor")
@@ -29,7 +29,7 @@ class compra(models.Model):
         EFECTIVO = 0, _("Efectivo")
         TRANSFERENCIA = 1, _("Transferencia")
         TARJETA = 2, _("Tarjetas Debito o Credito")
-        TRANSFERENCIABANCO =  3, _("Transferencia Bancaria")
+        TRANSFERENCIABANCARIA =  3, _("Transferencia Bancaria")
         CHEQUE = 4, _("Cheque")
     comp_medio_pago = models.CharField(max_length=1, choices=medPagoComp.choices, default=medPagoComp.EFECTIVO, verbose_name="Medio de pago de compra")
     class estCompra (models.IntegerChoices):
@@ -37,16 +37,13 @@ class compra(models.Model):
         INACTIVO = 0, _("Inactivo")
     comp_estado = models.CharField(max_length=1, choices=estCompra.choices, default=estCompra.ACTIVO, verbose_name="Estado de la compra")
     Usuario_usu_id = models.ForeignKey(Usuario, verbose_name="UsuarioCompID", on_delete=models.PROTECT, related_name='UsuarioComID')
-    Proveedor_prov_id = models.ForeignKey(proveedor, verbose_name="proveedor", on_delete=models.PROTECT, related_name='proveedor')
+    Proveedor_prov_id = models.ForeignKey(Proveedor, verbose_name="proveedor", on_delete=models.PROTECT, related_name='proveedor')
     
     
 class Detalle_compra(models.Model): 
-    comp_id = models.ForeignKey(compra, on_delete=models.PROTECT, verbose_name="Número de compra", related_name="Numcompra")
-    usu_id = models.ForeignKey(Usuario, on_delete=models.PROTECT, verbose_name="ID_usuario", related_name="ID_usuario")
-    det_comp_precio_unitario = models.IntegerField(verbose_name="Precio unitario del insumo")
-    det_comp_cantidad = models.IntegerField(verbose_name="Cantidad producto")
-    det_comp_precio_total = models.IntegerField(verbose_name="Compra, costo total")
-    class Estado(models.TextChoices):
-        ACTIVO='1', _('Activo')
-        INACTIVO='0', _('Inactivo')
-    estado=models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
+    det_comp_id = models.AutoField(primary_key=True)
+    det_comp_precio_unitario = models.IntegerField(verbose_name="Precio unitario")
+    det_comp_cantidad = models.IntegerField(verbose_name="Cantidad")
+    det_comp_precio_total = models.IntegerField(verbose_name="Procio total")
+    Insumos_insumos_id = models.ForeignKey(Insumo, verbose_name="id insumo")
+    Compra_comp_id = models.ForeignKey(Compra, verbose_name="Id Compra")
--- ventas/models.py
+++ ventas/models.py
@@ -1,47 +1,50 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from usuarios.models import Usuario
+from usuarios.models import Usuario, Servicio, Producto
 
 # Create your models here.
 class Cliente(models.Model): #Falta cliente_id autoinclementable
-    clie_nombre = models.CharField(max_length=45, verbose_name="Nombre Cliente")
-    clie_apellido = models.CharField(max_length=45, verbose_name="Apellido Cliente")
-    clie_telefono = models.CharField(max_length=15, verbose_name="Teléfono Cliente")
-    clie_direccion = models.CharField(max_length=45, verbose_name="Dirección Cliente")
-    class TipoDoc(models.IntegerChoices):
-        CC = 0, _('CC')
-        CE = 1, _('CE')
-        PEP = 2, _('PEP')
-        PA = 3, _('PA')
-        NIT = 4, _('NIT')
-        OT = 5, _('OTRO')
-    clie_tipo_documento = models.IntegerField(default=TipoDoc.CC, choices=TipoDoc.choices, verbose_name="Tipo documento")
-    clie_num_documento = models.CharField(max_length=25, verbose_name="Numero de documento")
+    clie_id = models.AutoField(primary_key=True) 
+    clie_nombre = models.CharField(max_length=45, verbose_name='Nombre cliente')
+    clie_apellido = models.CharField(max_length=45, verbose_name="Apellido proveedor")
+    clie_telefono = models.CharField(max_length=45, verbose_name="Telefono Cliente")
+    class tipoDoc (models.IntegerChoices):
+        CC = 0, _( "Cedula de Ciudadania")
+        CE = 1, _( "Cedula de Extranjeria")
+        PEP = 2, _( "Permiso de permanencia")
+        PA = 3, _( "Pasaporte")
+        NIT = 4, _( "Numero Identificacion Tributaria")
+        OTRO = 5, _( "Otro")
+    clie_tipo_documento = models.CharField(max_length=1, choices=tipoDoc.choices, default=tipoDoc.CC, verbose_name="Tipo de documento del cliente")
+    clie_num_documento = models.CharField(max_length=16, verbose_name="Numero documento cliente")
     clie_correo_electronico = models.CharField(max_length=45, verbose_name="Correo cliente")
     class Estado(models.TextChoices):
         ACTIVO='1', _('Activo')
         INACTIVO='0', _('Inactivo')
-    estado=models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
+    clie_estado = models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
     def __str__(self):
         return "%s %s" %(self.clie_nombre, self.clie_apellido)
     
 class Venta(models.Model):
-    usu_id = models.ForeignKey(Usuario, verbose_name="Usuario", on_delete=models.PROTECT, related_name='Usuario')
-    vent_fecha = models.DateField(verbose_name="Fecha de venta")
-    vent_precio_subtotal = models.IntegerField(verbose_name="Precio subtotal")
-    vent_iva = models.IntegerField(verbose_name="IVA")
-    vent_precio_total = models.IntegerField(verbose_name="Precio total")
+    vent_id = models.AutoField(primary_key=True)
+    vent_fecha = models.DateField(verbose_name="Fecha venta")
+    vent_cliente = models.CharField(max_length=45, verbose_name="Cliente")
+    vent_precio_subtotal = models.IntegerField(verbose_name="SubTotal venta")
+    vent_iva = models.IntegerField(verbose_name="Iva de la venta")
+    vent_precio_total = models.IntegerField(verbose_name="Precio total de la venta")
     class Estado(models.TextChoices):
-        ACTIVO='1', _('Activo')
-        INACTIVO='0', _('Inactivo')
-    estado=models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
+        ACTIVO ='1', _('Activo')
+        INACTIVO ='0', _('Inactivo')
+    vent_estado = models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
+    Cliente_clie_id = models.ForeignKey(Cliente, verbose_name="Id cliente")
+    Usuario_usu_id = models.ForeignKey(Usuario, verbose_name="Id Usuario")
+    
     
 class Detalle_Venta(models.Model): #Falta venta_has_clientecol y venta_vent_id "autoincrementable"
-    vent_id = models.ForeignKey(Venta, on_delete=models.PROTECT, verbose_name="Número de venta", related_name="NumVenta")
-    cliente_clie_id = models.ForeignKey(Cliente, on_delete=models.PROTECT, verbose_name="Número de cliente", related_name="NumCliente")
-    det_vent_precio_unitario = models.IntegerField(verbose_name="Precio unitario de producto")
-    det_vent_cantidad = models.IntegerField(verbose_name="Cantidad producto")
-    class Estado(models.TextChoices):
-        ACTIVO='1', _('Activo')
-        INACTIVO='0', _('Inactivo')
-    estado=models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
+    det_vent_id = models.AutoField(primary_key=True)
+    det_vent_precio_unitario = models.IntegerField(verbose_name="Precio unitario del producto")
+    det_vent_cantidad = models.IntegerField(verbose_name="Cantidad vendida")
+    det_vent_precio_total = models.IntegerField(verbose_name="Precio total del producto")
+    Servicios_serv_id = models.ForeignKey(Servicio, null=True, verbose_name="Id Servicio")
+    Producto_producto_id = models.ForeignKey(Producto, null=True, verbose_name="Id Producto")
+    Venta_vent_id = models.ForeignKey(Venta,  verbose_name="Id Venta")
